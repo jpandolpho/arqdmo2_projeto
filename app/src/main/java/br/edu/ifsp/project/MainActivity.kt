e8f0e4b0c95921a6aee1301286743dc42407920b
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-03-24 10:47:36 -0300

    Adicionando visualização de imagem.

--- app/src/main/java/br/edu/ifsp/project/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/project/MainActivity.kt
@@ -1,10 +1,13 @@
 package br.edu.ifsp.project
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.ImageRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
@@ -14,8 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView: TextView = findViewById(R.id.textView)
+        val imageView: ImageView = findViewById(R.id.imageView)
+
         val queue: RequestQueue = Volley.newRequestQueue(this)
         val urlTexto = "http://10.105.68.96:8080/exemplo/texto"
+        val urlImagem = "http://10.105.68.96:8080/exemplo/imagem"
+
         val stringRequest = StringRequest(
             Request.Method.GET, urlTexto,
             { response ->
@@ -25,6 +32,17 @@ class MainActivity : ComponentActivity() {
             { error ->
                 error.printStackTrace()
             })
+        val imageRequest = ImageRequest(urlImagem,
+            { response ->
+            // Exibir a imagem recebida
+                imageView.setImageBitmap(response)
+            },
+            0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
+            { error ->
+                error.printStackTrace()
+            })
+
         queue.add(stringRequest)
+        queue.add(imageRequest)
     }
 }
