e4808b97c741b2f2e72048c90182b78eda1c6f32
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-03-24 11:10:12 -0300

    Criando botão para popular a tela.

--- app/src/main/java/br/edu/ifsp/project/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/project/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import br.edu.ifsp.project.databinding.ActivityMainBinding
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.ImageRequest
@@ -12,37 +13,54 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textView)
-        val imageView: ImageView = findViewById(R.id.imageView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        configButton()
+    }
+
+    private fun configButton() {
+        binding.buttonGerar.setOnClickListener {
+            populate()
+        }
+    }
+
+    private fun populate() {
         val queue: RequestQueue = Volley.newRequestQueue(this)
-        val urlTexto = "http://10.105.68.96:8080/exemplo/texto"
-        val urlImagem = "http://10.105.68.96:8080/exemplo/imagem"
+        val stringRequest = createRequestString()
+        val imageRequest = createRequestImage()
+        queue.add(stringRequest as Request<*>?)
+        queue.add(imageRequest as Request<*>?)
+    }
 
-        val stringRequest = StringRequest(
+    private fun createRequestString(): Any {
+        val urlTexto = "http://10.105.68.96:8080/exemplo/texto"
+        return StringRequest(
             Request.Method.GET, urlTexto,
             { response ->
-        // Exibir o texto recebido
-                textView.text = response
+                // Exibir o texto recebido
+                binding.textView.text = response
             },
             { error ->
                 error.printStackTrace()
             })
-        val imageRequest = ImageRequest(urlImagem,
+    }
+
+    private fun createRequestImage() : Any{
+        val urlImagem = "http://10.105.68.96:8080/exemplo/imagem"
+        return ImageRequest(urlImagem,
             { response ->
-            // Exibir a imagem recebida
-                imageView.setImageBitmap(response)
+                // Exibir a imagem recebida
+                binding.imageView.setImageBitmap(response)
             },
             0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
             { error ->
                 error.printStackTrace()
             })
-
-        queue.add(stringRequest)
-        queue.add(imageRequest)
     }
 }
